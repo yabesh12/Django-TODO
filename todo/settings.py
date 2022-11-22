bf698db80bc77ac8516af70ad1ac1d44a2e95905
Yabesh <yabesh@deepsense.in>
2022-11-22 17:51:40 +0530

    Commit added for deployment

--- todo/settings.py
+++ todo/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://406d-223-226-4-20.ngrok.io']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -127,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
